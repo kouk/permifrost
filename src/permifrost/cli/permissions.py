6c3adf9c28c682d8e2efc9649ddc16d792454664
Thomas La Piana <tal103020@icloud.com>
2020-09-14 21:16:41 +0700

    linted

--- src/permifrost/cli/permissions.py
+++ src/permifrost/cli/permissions.py
@@ -39,7 +39,7 @@ def print_command(command, diff):
 @click.argument("spec")
 @click.option("--dry", help="Do not actually run, just check.", is_flag=True)
 @click.option(
-    "--role", help="Only run commands pertaining to a specific role.", is_flag=True,
+    "--role", help="Only run commands pertaining to a specific role.", is_flag=True
 )
 @click.option(
     "--diff", help="Show full diff, both new and existing permissions.", is_flag=True
