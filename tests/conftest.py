fc54a96107ca93fe7c529a4833dd26c3b570414e
Blake <blake.enyart@gmail.com>
2021-09-11 11:36:47 -0600

    Integrate docstrings into pytests output
    Add test suite for YAML file loading

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import os
 import logging
+from colorama import init, Fore, Style
+init()
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,3 +16,15 @@ def concurrency():
         "processes": int(os.getenv("PYTEST_CONCURRENCY_PROCESSES", 8)),
         "cases": int(os.getenv("PYTEST_CONCURRENCY_CASES", 64)),
     }
+
+def pytest_itemcollected(item):
+    """
+    Brought in for docstring output:
+    https://stackoverflow.com/a/39035226
+    """
+    par = item.parent.obj
+    node = item.obj
+    pref = par.__doc__.strip() + ' ' if par.__doc__ else ''
+    suf = node.__doc__.strip() if node.__doc__ else ''
+    if pref or suf:
+        item._nodeid = Fore.YELLOW + ''.join((pref, suf)) + '\n' + Style.RESET_ALL + item._nodeid
