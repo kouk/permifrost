02724ccc7e028a9bb057be6460ed72be922ffb76
Blake <blake.enyart@gmail.com>
2021-09-22 18:19:11 -0600

    Flattens docstring multispace and newlines

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,11 +26,11 @@ def pytest_itemcollected(item):
     """
     par = item.parent.obj
     node = item.obj
-    pref = par.__doc__.strip() + " " if par.__doc__ else ""
+    pref = " ".join(par.__doc__.split()) + " " if par.__doc__ else ""
     try:
         # Enables docstrings for class, methods, functions passed in mark.parametrize
         # to be concatenated and output to the CLI when pytest is run
-        suf = node.__doc__.strip() + " " if node.__doc__ else ""
+        suf = " ".join(node.__doc__.split()) + " " if node.__doc__ else ""
         if "[" in item._nodeid:
             mark_list = item._nodeid.split("[")[-1][:-1].split("-")
         else:
@@ -40,13 +40,12 @@ def pytest_itemcollected(item):
                 for param in pytest_mark.args[1]:
                     # Works for functions passed into mark.parametrize
                     if param.__name__ in mark_list:
-                        clean_doc = param.__doc__.strip()
-                        clean_doc = " ".join(clean_doc.split())
+                        clean_doc = " ".join(param.__doc__.split())
                         suf += clean_doc + " "
     except AttributeError:
-        suf = node.__doc__.strip() + " " if node.__doc__ else ""
+        suf = suf = " ".join(node.__doc__.split()) + " " if node.__doc__ else ""
     except Exception:
-        suf = node.__doc__.strip() + " " if node.__doc__ else ""
+        suf = suf = " ".join(node.__doc__.split()) + " " if node.__doc__ else ""
     if pref or suf:
         item._nodeid = (
             Fore.YELLOW + "".join((pref, suf)) + "\n" + Style.RESET_ALL + item._nodeid
