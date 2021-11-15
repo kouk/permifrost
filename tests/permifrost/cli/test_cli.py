43cc523c048b7e8f2000a5cecfd804491aeebba4
Paul Armstrong <parmstrong@gitlab.com>
2021-11-15 19:40:05 +0200

    Corrected method of retrieving variables in tests

--- tests/permifrost/cli/test_cli.py
+++ tests/permifrost/cli/test_cli.py
@@ -1,6 +1,5 @@
 import permifrost
 from permifrost.cli import cli
-from permifrost.cli.permissions import run, spec_cli_test
 
 
 def test_version(cli_runner):
@@ -9,12 +8,12 @@ def test_version(cli_runner):
 
 
 def test_run_command(cli_runner):
-    cli_version = cli_runner.invoke(run, ["--help"])
+    cli_version = cli_runner.invoke(cli.commands["run"], ["--help"])
     cli_output = cli_version.output
     assert (len(cli_output) >= 5) and (cli_output[:5] == "Usage")
 
 
 def test_load_command(cli_runner):
-    cli_version = cli_runner.invoke(spec_cli_test, ["--help"])
+    cli_version = cli_runner.invoke(cli.commands["spec-cli-test"], ["--help"])
     cli_output = cli_version.output
     assert (len(cli_output) >= 5) and (cli_output[:5] == "Usage")
