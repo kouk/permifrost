2713d90165fa329348610536dc6d74ab85fbbf1e
Paul Armstrong <parmstrong@gitlab.com>
2021-11-15 19:36:21 +0200

    Added tests to confirm run and spec cli commands are working as expected.

--- tests/permifrost/cli/test_cli.py
+++ tests/permifrost/cli/test_cli.py
@@ -1,8 +1,20 @@
 import permifrost
 from permifrost.cli import cli
+from permifrost.cli.permissions import run, spec_cli_test
 
 
 def test_version(cli_runner):
     cli_version = cli_runner.invoke(cli, ["--version"])
-
     assert cli_version.output == f"permifrost, version {permifrost.__version__}\n"
+
+
+def test_run_command(cli_runner):
+    cli_version = cli_runner.invoke(run, ["--help"])
+    cli_output = cli_version.output
+    assert (len(cli_output) >= 5) and (cli_output[:5] == "Usage")
+
+
+def test_load_command(cli_runner):
+    cli_version = cli_runner.invoke(spec_cli_test, ["--help"])
+    cli_output = cli_version.output
+    assert (len(cli_output) >= 5) and (cli_output[:5] == "Usage")
